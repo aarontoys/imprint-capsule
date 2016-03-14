edff50023cd393f943c72b9381909d4febcac934
Andy Gross <andogro@gmail.com>
2016-03-14 13:31:02 -0600

    added places and user view folders

--- src/server/routes/profile.js
+++ src/server/routes/profile.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'profile' });
+router.get('/:id', function(req, res, next) {
+  var id = req.params.id;
+  res.render('profile', { title: 'User Profile' });
 });
 
 module.exports = router;
