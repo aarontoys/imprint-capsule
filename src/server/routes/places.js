42b092c43ed88f290c39fe3dde3194b884849209
rlgaines <robertlg35@gmail.com>
2016-03-14 16:36:07 -0600

    merged routes

--- src/server/routes/places.js
+++ src/server/routes/places.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('places', { title: 'All Places' });
+  res.render('places/all', { title: 'All Places' });
 });
 
 router.get('/:id', function(req, res, next) {
-  res.render('index', { title: 'All Places' });
+  res.render('places/single', { title: 'All Places' });
 });
 
 module.exports = router;
