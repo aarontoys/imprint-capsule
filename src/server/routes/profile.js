06937ae399f2e2ac38484f89be48120d1699e1f5
Andy Gross <andogro@gmail.com>
2016-03-14 14:42:40 -0600

    adding user forms

--- src/server/routes/profile.js
+++ src/server/routes/profile.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/:id/new', function(req, res, next) {
+  var id = req.params.id;
+  res.render('users/new', { title: 'Add User Profile' });
+});
+
+router.get('/:id/edit', function(req, res, next) {
+  var id = req.params.id;
+  res.render('users/edit', { title: 'Edit User Profile' });
+});
+
 router.get('/:id', function(req, res, next) {
   var id = req.params.id;
-  res.render('profile', { title: 'User Profile' });
+  res.render('users/profile', { title: 'Edit User Profile'+id });
 });
+
+
+
+
+
+
+
 
 module.exports = router;
