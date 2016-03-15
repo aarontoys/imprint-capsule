98e15edde42af634a3b7f1fa000add5268c5086d
Kyle Maxwell <kylejaymaxwell@gmail.com>
2016-03-15 10:23:39 -0600

    user_types seeded with admin, event creator, and user

---
+++ seeds/user_types.js
@@ -0,0 +1,22 @@
+
+exports.seed = function(knex, Promise) {
+  return Promise.join(
+    // Deletes ALL existing entries
+    knex('user_types').del(), 
+
+    // Inserts seed entries
+    knex('user_types').insert(
+        {
+          u_type: 'Admin'
+        }),
+    knex('user_types').insert(
+        {
+          u_type: 'Event Creator'
+        }),
+    knex('user_types').insert(
+        {
+          u_type: 'User'
+        })
+
+    );
+};
