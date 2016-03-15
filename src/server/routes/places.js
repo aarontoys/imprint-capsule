ae56af3db1ac7da19b8add5a225b90e5e919984d
Kyle Maxwell <kylejaymaxwell@gmail.com>
2016-03-15 15:46:05 -0600

    geocoding an address. with removal of api key

--- src/server/routes/places.js
+++ src/server/routes/places.js
@@ -3,10 +3,17 @@ var router = express.Router();
 var pg = require('pg');
 var connectionString = 'postgres://localhost:5432/imprints';
 var knex = require('../../../db/knex');
-var queries = require('./queries')
+var queries = require('./queries');
+
 
 router.get('/', function(req, res, next) {
   res.render('places/all', { title: 'All Places' });
+});
+
+router.get('/geo', function(req, res, next) {
+  var key = process.env.MAPS;
+  console.log(key);
+  res.render('places/geo', { title: 'Geo test', api: key});
 });
 
 router.get('/create', function(req, res, next) {
