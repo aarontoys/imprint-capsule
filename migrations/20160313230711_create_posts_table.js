1336affe2ea0c00816fad8879faf59aa61b4139b
Aaron Toys <aarontoys@gmail.com>
2016-03-15 13:47:44 -0600

    updated migrations - youll need to rollback and then re-run migrations

--- migrations/20160313230711_create_posts_table.js
+++ migrations/20160313230711_create_posts_table.js
@@ -2,7 +2,7 @@
 exports.up = function(knex, Promise) {
     return knex.schema.createTable('posts', function(table){
     table.increments('p_id');
-    table.string('post');
+    table.text('post');
     table.string('p_img');
     table.integer('e_id').references('e_id').inTable('events');
     table.integer('u_id').references('u_id').inTable('users');
