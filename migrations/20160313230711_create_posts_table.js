aa25de9716aedddf9c912ea55467b516d0cee176
Aaron Toys <aarontoys@gmail.com>
2016-03-16 09:41:37 -0600

    updated knex migration files to include additional fields

--- migrations/20160313230711_create_posts_table.js
+++ migrations/20160313230711_create_posts_table.js
@@ -3,6 +3,7 @@ exports.up = function(knex, Promise) {
     return knex.schema.createTable('posts', function(table){
     table.increments('p_id');
     table.text('post');
+    table.timestamp('created_at').defaultTo(knex.fn.now());
     table.string('p_img');
     table.integer('e_id').references('e_id').inTable('events');
     table.integer('u_id').references('u_id').inTable('users');
