b11a609e9f1ceb3472468d0cf79463763c8c2b2d
Andy Gross <andogro@gmail.com>
2016-03-17 07:11:03 -0600

    updated styles on visitors, added login route

--- src/server/routes/register.js
+++ src/server/routes/register.js
@@ -25,4 +25,9 @@ router.get('/', function(req, res, next) {
 	});
 });
 
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Login' });
+});
+
+
 module.exports = router;
