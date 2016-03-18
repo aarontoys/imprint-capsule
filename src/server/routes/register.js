d3c70b6d1b208082c368c7251e772f11e59955f5
Aaron Toys <aarontoys@gmail.com>
2016-03-17 21:23:31 -0600

    removed login box from homepage when logged in, redirect to previous page after loggin in

--- src/server/routes/register.js
+++ src/server/routes/register.js
@@ -26,9 +26,9 @@ router.get('/', function(req, res, next) {
 	});
 });
 
-router.get('/login', function(req, res, next) {
-  res.render('login', { title: 'Login' });
-});
+// router.get('/login', function(req, res, next) {
+//   res.render('login', { title: 'Login' });
+// });
 
 
 module.exports = router;
